ebd444cd4a7334ac4b6e9d2f399a08c6f647f899
Martial Salgues <github@mar7ius.me>
2022-08-11 18:45:04 +0200

    rename to run after packages are installed

--- prefs/.chezmoiscripts/run_onchange_before_02-decrypt-private-key.sh
+++ prefs/.chezmoiscripts/run_onchange_before_02-decrypt-private-key.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+echo "Checking for age key..."
+
 if [ ! -f "${HOME}/.age_key" ]; then
+    echo "key no found, retrieving..."
     chezmoi execute-template '{{ bitwardenAttachment "key.txt.age" .personal.bw_vault.items.age_key_file_id }}' > "${HOME}/key.txt.age"
     age --decrypt --output "${HOME}/.age_key" "${HOME}/key.txt.age"
     chmod 600 "${HOME}/.age_key"
     rm -f "${HOME}/key.txt.age"
 fi
+
+echo "done"
