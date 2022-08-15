c47541f3f1b3e98264c37f8c9aa8809bb748f907
Martial Salgues <github@mar7ius.me>
2022-08-15 16:45:02 +0200

    use template utilities

--- prefs/.chezmoiscripts/run_onchange_before_02-decrypt-private-key.sh
+++ prefs/.chezmoiscripts/run_onchange_before_02-decrypt-private-key.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-echo "Checking for age key..."
+{{ template "source-utils" . }}
+
+section "Setting up age encryption"
+action "Checking for age key..."
 
 if [ ! -f "${HOME}/.age_key" ]; then
-    echo "key no found, retrieving..."
+    log "key no found, retrieving..."
     chezmoi execute-template '{{ bitwardenAttachment "key.txt.age" .personal.bw_vault.items.age_key_file_id }}' > "${HOME}/key.txt.age"
     age --decrypt --output "${HOME}/.age_key" "${HOME}/key.txt.age"
     chmod 600 "${HOME}/.age_key"
     rm -f "${HOME}/key.txt.age"
 fi
 
-echo "done"
+success "Age is ready"
